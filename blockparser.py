60f07f3b8132003739066eae49ff8d9756ff5245
manfromsouth <igor.bordonos@gmail.com>
2020-08-17 12:50:51 +0300

    Commit 6 - Reworked encryption process (not the encryption algorithm). Added commentaries for fileoperations.py

--- blockparser.py
+++ blockparser.py
@@ -1,5 +1,4 @@
 # Разбивает текст на набор блоков
-
 class BlockParser:
     def __init__(self, block_size=8, encoding='utf-8'):
         """
@@ -18,7 +17,6 @@ class BlockParser:
         преобразует строку в набор блоков размером в block_size байт и добавляет набор в список __result,
         в процессе удаляет из нее последний переход на новую строку
         :param line: преобразуемая строка
-        :return:
         """
         # представление строки в байтовом виде
         byte_string = bytearray(line, self.__encoding)
@@ -52,9 +50,6 @@ class BlockParser:
     def get_result(self):
         return self.__result.copy()
 
-    def get_added_bytes(self):
-        return self.__added_bytes.copy()
-
     def rebuild_line(self):
         """
         достает строчки из аттрибутов __result и __added_bytes по принципу FIFO
@@ -64,7 +59,6 @@ class BlockParser:
         if len(self.__result) > 0:
             # получение первых элементов списков - аттрибутов
             parsed_string = self.__result[0]
-            added_bytes = self.__added_bytes[0]
             # обрубание первых элементов списков - аттрибутов
             self.__result = self.__result[1:]
             # self.__added_bytes = self.__added_bytes[1:]
@@ -73,30 +67,40 @@ class BlockParser:
             for block in parsed_string:
                 byte_string += block
             full_string = byte_string.decode(self.__encoding, 'replace')
-            return full_string.rstrip()
-
+            if len(full_string.rstrip()) > 0:
+                return full_string.rstrip()
+            else:
+                return None
         else:
             return None
 
-    # заполняет данные о блоках и добавленных в них байтах
-    def load_lines(self, lines_list, bytes_list):
+    # заполняет данные о блоках
+    def load_lines(self, lines_list):
         self.__result = lines_list.copy()
-        self.__added_bytes = bytes_list.copy()
 
-    # чистит спсики блоков и добавленных байтов
+    # чистит спсики блоков
     def clear_results(self):
         self.__result = list()
-        self.__added_bytes = list()
 
     def replace_block(self, line_num, block_num, block):
+        """
+        Заменяет указанный блок переданным
+        :param line_num: номер строки
+        :param block_num: номер блока в строке
+        :param block: замена для блока
+        """
         self.__result[line_num][block_num] = block.copy()
 
     def load_array(self, array):
+        """
+        Загружает байтовый массив через parse_line, попутно, разбив его на строки
+        :param array:
+        :return:
+        """
         self.clear_results()
         string_array = array.decode(self.__encoding, 'ignore').split('\n')
         for line in string_array:
             self.parse_line(line)
-
 
 
 if __name__ == '__main__':
--- fileoperations.py
+++ fileoperations.py
@@ -2,7 +2,7 @@ from blockparser import BlockParser
 from encrypt import Encryptor
 
 
-class File_operator():
+class FileOperator:
 
     def __init__(self, file_name):
         # имя файла
@@ -10,7 +10,13 @@ class File_operator():
         # данные в файле
         self.__parser = BlockParser()
 
-    def load_encrypted_file_into_memory(self, key = None):
+    def load_encrypted_file_into_memory(self, key=None):
+        """
+        Загружает данные из файла в парсер
+        если Key - не None, то перед этим происходит дешифровка
+        считанных данных по этому ключу
+        :param key: Ключ шифрования
+        """
         with open(self.file_name, 'rb') as f:
             operational_data = bytearray(f.read())
         if key is not None:
@@ -20,31 +26,39 @@ class File_operator():
                     operational_data[pointer*8:pointer*8+8])
         self.__parser.load_array(operational_data)
 
-
-    def load_data_into_file(self):
+    def load_data_into_file(self, key=None):
+        """
+        Загружает данные из парсера в файл.
+        Если key - не None, то данные перед этим шифруются
+        :param key:  Ключ шифрования
+        """
+        if key is not None:
+            self.__encrypt_decrypt_data_in_memory(key)
         result = self.__parser.get_result()
         with open(self.file_name, 'wb') as f:
             for line in result:
                 for block in line:
-                    f.write((block))
-
-        '''
-        try:
-            dp_line = self.__parser.rebuild_line().encode('utf-8')
-            with open(self.file_name, 'wb') as f:
-                while dp_line > b'':
-                    f.write(dp_line)
-                    dp_line = self.__parser.rebuild_line().encode('utf-8')
-        except AttributeError:
-            pass'''
+                    f.write(block)
 
     def add_line_to_memory(self, line):
+        """
+        Записывает строку в парсер
+        :param line:  записываемая строка
+        """
         self.__parser.parse_line(line)
 
     def get_data_from_memory(self):
+        """
+        Считывает весь массив байтов в парсере
+        :return: массив байтов
+        """
         return self.__parser.get_result()
 
-    def encrypt_decrypt_data_in_memory(self, key):
+    def __encrypt_decrypt_data_in_memory(self, key):
+        """
+        По ключу зашифровывает данные в парсере
+        :param key: Ключ шифрования
+        """
         current_encryptor = Encryptor(key)
         current_encryptor.drop_key_pointer()
         data_snapshot = self.__parser.get_result()
@@ -53,17 +67,28 @@ class File_operator():
                 self.__parser.replace_block(line_num, block_num,
                                             current_encryptor.encrypt_decrypt_block(block))
 
+    def extract_readable_data(self):
+        """
+        Удаляет все данные из блоков, возвращая их в пригодном для использования виде
+        :return: список строк
+        """
+        result_list = list()
+        dp_line = self.__parser.rebuild_line()
+        while dp_line is not None:
+            result_list.append(dp_line)
+            dp_line = self.__parser.rebuild_line()
+        return result_list
 
 
 if __name__ == '__main__':
 
-    file = File_operator('test_file.txt')
+    file = FileOperator('test_file.txt')
     file.add_line_to_memory('this is a test line')
     file.add_line_to_memory('and this too')
     file.add_line_to_memory('а это на Русском')
     print(file.get_data_from_memory())
-    file.encrypt_decrypt_data_in_memory('Marx')
-    file.load_data_into_file()
+    file.load_data_into_file('Marx')
     file.load_encrypted_file_into_memory('Marx')
     # file.encrypt_decrypt_data_in_memory('Marx')
     print(file.get_data_from_memory())
+    print(file.extract_readable_data())
